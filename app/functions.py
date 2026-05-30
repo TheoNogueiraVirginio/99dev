d15e089e7fc4f1a38bdd298b3d234b926138ceca
jftigre <joaofelipetigre@gmail.com>
2026-05-30 10:51:47 -0300

    feat(cadastro): validar rotas e aplicar hash bcrypt nas senhas

--- app/functions.py
+++ app/functions.py
@@ -1,13 +1,22 @@
 from flask_mail import Message
-
+import bcrypt
 from app import mail, serializer
 from app.models import Usuario, db
 
 
 def cadastrar_usuario(email, senha, cargo_definido):
+    usuario_existente = Usuario.query.filter_by(email=email).first()
+    if usuario_existente:
+        raise ValueError("Este e-mail já está cadastrado no sistema.")
+
+    #Criptografia
+    senha_bytes = senha.encode('utf-8')
+    salt = bcrypt.gensalt()
+    senha_hash = bcrypt.hashpw(senha_bytes, salt)
+    
     novo_usuario = Usuario(
         email=email,
-        senha=senha,
+        senha=senha_hash.decode('utf-8'), 
         cargo=cargo_definido
     )
     db.session.add(novo_usuario)
@@ -18,6 +27,17 @@ def cadastrar_usuario(email, senha, cargo_definido):
         raise
 
     return novo_usuario
+
+def autenticar_usuario(email,senha):
+    usuario = Usuario.query.filter_by(email=email).first()
+
+    if not usuario:
+        raise ValueError("Usuário não encontrado")
+    
+    if usuario.senha != senha:
+        raise ValueError("Senha incorreta")
+    
+    return usuario
 
 def solicitar_recuperacao_senha(email):
     usuario = Usuario.query.filter_by(email=email).first()
@@ -63,7 +83,13 @@ def atualizar_senha(email, nova_senha):
     usuario = Usuario.query.filter_by(email=email).first()
     if not usuario:
         raise ValueError("Usuário não encontrado")
-    usuario.senha = nova_senha
+    
+    #Criptografia redefinição de senha
+    senha_bytes = nova_senha.encode('utf-8')
+    salt = bcrypt.gensalt()
+    senha_hash = bcrypt.hashpw(senha_bytes, salt)
+    
+    usuario.senha = senha_hash.decode('utf-8')
     try:
         db.session.commit()
     except Exception:
